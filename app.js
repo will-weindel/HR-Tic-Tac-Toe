fafc87b17c681adaa112f3db148ccb1392572171
Will Weindel <will.weindel@gmail.com>
2021-05-24 10:03:47 -0700

    adds class to game squares, adds event listener, begins to setup logic for X or O add

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('Hello World');
+
+//setup intial game-board
+function initialize() {
+  var gameSquares = document.querySelectorAll('td');
+
+  gameSquares.forEach((el) => {
+    el.classList.add('game-square');
+    el.addEventListener('click', () => {
+      el.innerText = getNextXorO(el);
+    })
+  });
+}
+
+function getNextXorO(element) {
+  console.log(element);
+}
+
+
+
+initialize();
